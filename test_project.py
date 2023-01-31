0cb20d0ae5b95d26c9bb1dfe5c63764a890d15b0
Khalid <khalidazhmd@gmail.com>
2023-01-31 08:10:15 +0300

    removed unnecessary func call in test file

--- test_project.py
+++ test_project.py
@@ -9,7 +9,10 @@ def test_get_weekly_avg():
     assert get_weekly_avg([None, 77, 73, 75, 74, None, 76]) == [75.0]
     assert get_weekly_avg([93.5, 92.1, 88.7, 89.3, 90.2, 91.2, 87]) == [90.3]
     # average weight for one week and 3 days
-    assert get_weekly_avg([93.5, 92.1, 88.7, 89.3, 90.2, 91.2, 87, 76, 72, 74]) == [90.3, 74.0]
+    assert get_weekly_avg([93.5, 92.1, 88.7, 89.3, 90.2, 91.2, 87, 76, 72, 74]) == [
+        90.3,
+        74.0,
+    ]
     # average weight for 3 weeks
     assert get_weekly_avg(
         [
@@ -36,9 +39,6 @@ def test_get_weekly_avg():
             76,
         ]
     ) == [90.3, 0, 75.0]
-
-
-test_get_weekly_avg()
 
 
 def test_function_2():
